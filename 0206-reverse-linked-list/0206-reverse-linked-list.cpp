e460e340e6b60be5a01cd9792dfc50a9507206da
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2023-09-11 22:10:17 +0900

    Time: 4 ms (74.8%), Space: 8.4 MB (37.25%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,14 +8,23 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- //recursive
+//iteratively
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         if(head==NULL || head->next==NULL) return head;
-        ListNode* newhead=reverseList(head->next);
-        head->next->next=head;
+        ListNode* p1=head;
+        ListNode* p2=head->next;
+        ListNode* p3=p2->next;
+        
+        while(p3){
+            p2->next=p1;
+            p1=p2;
+            p2=p3;
+            p3=p3->next;
+        }
+        p2->next=p1;
         head->next=NULL;
-        return newhead;
+        return p2;
     }
 };
