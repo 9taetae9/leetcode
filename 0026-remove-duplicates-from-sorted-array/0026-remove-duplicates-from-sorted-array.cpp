04a0ac2ae42c100c7cea65fb61ed931591047d06
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2023-09-13 03:29:55 +0900

    Time: 12 ms (35.89%), Space: 18.4 MB (64.35%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        set<int> s;
-        for(auto e:nums){
-            s.insert(e);
+        if(nums.size() == 0) return 0;  // handle edge case
+        
+        int pos = 0;  // position to insert next unique number
+        
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] != nums[pos]) {
+                pos++;
+                nums[pos] = nums[i];
+            }
         }
-        int i=0;
-        for(auto e:s){
-            nums[i++]=e;
-        }
-        return s.size();
+        return pos + 1;
     }
 };
