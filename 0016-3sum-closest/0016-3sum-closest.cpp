5131ebb323f20b0c389278b66f1d939c38a2adb0
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2023-09-17 22:15:30 +0900

    Time: 41 ms (41.83%), Space: 10.5 MB (9.71%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -2,26 +2,29 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        int result; //for the closest sum
-        int min_distance=20000; //minimum dist
-        int sum;
-        for(int i=0; i<nums.size(); i++){
-            int left=i+1,right=nums.size()-1;
-            while(left<right){
-                sum=nums[i]+nums[left]+nums[right];
-                if(sum==target) return sum;
-                int distance=abs(target-sum);
-                if(min_distance>distance) {
-                    min_distance=distance;
-                    result=sum;
-                    if(sum>target) right--;
-                    else left++;
-                }else{
-                    if(sum>target) right--;
-                    else left++; 
+        int result = nums[0] + nums[1] + nums[2]; // Initialize with first three elements
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                // Update result if sum is closer to target than previous result
+                if (abs(target - sum) < abs(target - result)) {
+                    result = sum;
                 }
-            }  
+
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
         }
-        return result;  
+
+        return result;
     }
 };
