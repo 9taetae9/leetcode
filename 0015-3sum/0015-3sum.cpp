c0bf7d8b61a21bd12f7063c06264e72b7fb26317
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2023-09-17 21:07:11 +0900

    Time: 128 ms (62.62%), Space: 30.2 MB (39.12%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        vector<int> tri_v;
+        set<vector<int>> tri_set;
+        
+        sort(nums.begin(), nums.end());//to use two point approach more effciently.
+        for(int i=0; i<nums.size(); i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;//skip duplicates of nums[i]
+            int left=i+1, right=nums.size()-1;//size()-1 is maximum valid index! 
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum==0){
+                tri_v.push_back(nums[i]);
+                tri_v.push_back(nums[left]);
+                tri_v.push_back(nums[right]);
+                //sort(tri_v.begin(), tri_v.end());
+                tri_set.insert(tri_v);
+                tri_v.clear();
+
+                while(left<right && nums[left]==nums[left+1]) left++;
+                while(left<right && nums[right]==nums[right-1]) right--;
+                left++;
+                right--;
+            } else if(sum<0){
+                left++; 
+            } else{
+                right--; 
+            }
+            }
+        }
+        vector<vector<int>> tri_vec(tri_set.begin(), tri_set.end());
+        return tri_vec;
+    }
+};
