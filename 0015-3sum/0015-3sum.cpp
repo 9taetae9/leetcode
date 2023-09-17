5ce325a731114d5a801fa0cdf447c30c06a849c5
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2023-09-17 21:15:18 +0900

    Time: 134 ms (60.49%), Space: 30.1 MB (39.21%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -19,8 +19,8 @@ public:
                 tri_set.insert(tri_v);
                 tri_v.clear();
 
-                while(left<right && nums[left]==nums[left+1]) left++;
-                while(left<right && nums[right]==nums[right-1]) right--;
+                //while(left<right && nums[left]==nums[left+1]) left++;
+                //while(left<right && nums[right]==nums[right-1]) right--;
                 left++;
                 right--;
             } else if(sum<0){
