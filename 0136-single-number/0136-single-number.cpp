c043a67548f0849c210164f5008ac072e205ad6c
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2023-09-18 21:47:37 +0900

    Time: 23 ms (25.88%), Space: 20.5 MB (8.31%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int output=0;
-        for(auto v: nums){
-            output^=v;
-        }
-        return output;
+        unordered_map<int,int> a;
+        for(auto x: nums)
+        a[x]++;
+        for(auto z:a)
+        if(z.second==1)
+        return z.first;
+        return -1;
     }
 };
 
