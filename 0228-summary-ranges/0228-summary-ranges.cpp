98963d244e081f4e1848cb575ab75a91f6d06cc4
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2023-09-20 03:49:45 +0900

    Time: 0 ms (100%), Space: 7.1 MB (6.76%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -2,22 +2,29 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> s;
-        if(nums.size()==0) return s;
+        if(nums.empty()) return s;
+        
+        int range_start = nums[0], range_end = nums[0];
 
-        int range_start=nums[0], range_end=nums[0];
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i-1]+1==nums[i]) range_end=nums[i];
-            else {
-                if(range_start==range_end) s.push_back(to_string(range_start));
-                else s.push_back(to_string(range_start)+"->"+to_string(range_end));
-                range_start=nums[i];
-                range_end=nums[i];
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i-1] + 1 == nums[i]) {
+                range_end = nums[i];
+            } else {
+                addRange(s, range_start, range_end);
+                range_start = nums[i];
+                range_end = nums[i];
             }
         }
-
-        if(range_start==range_end) s.push_back(to_string(range_start));
-        else s.push_back(to_string(range_start)+"->"+to_string(range_end));
+        addRange(s, range_start, range_end);
         return s;
+    }
 
+private:
+    void addRange(vector<string>& s, int range_start, int range_end) {
+        if(range_start == range_end) {
+            s.push_back(to_string(range_start));
+        } else {
+            s.push_back(to_string(range_start) + "->" + to_string(range_end));
+        }
     }
 };
