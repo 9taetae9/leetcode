4f466fbb56402ba45638805c15a948fe8f0e7d4d
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2023-09-20 17:11:48 +0900

    Update 0012-integer-to-roman.cpp
    better approach.

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,49 +1,19 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<string> v;
-        string temp;
-        int pos=0;
-        int i=0;
-        char c[4]={'I','X','C','M'};
-        char k[3]={'V','L','D'};
-        while(num){
-            int n = num%10; pos++;  //일의 자리부터 구하기 시작 일,십,백,천 1,2,3,4
-            if(n==4||n==9){
-                temp+=c[pos-1];
-                if(n==4)
-                    temp+=k[pos-1];
-                else 
-                    temp+=c[pos];
-
-                v.push_back(temp);
-                temp.clear();
+        // Create vectors of possible numerals and their integer values
+        vector<string> numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        
+        string result = "";
+        
+        for (int i = 0; i < values.size(); ++i) {
+            while (num >= values[i]) {
+                result += numerals[i];
+                num -= values[i];
             }
-            else if(0<=n&&n<=3){
-                while(i!=n){
-                    temp+=c[pos-1];
-                    i++;
-                }
-                v.push_back(temp);
-                temp.clear();
-                i=0;
-            }
-            else{
-                temp+=k[pos-1];
-                while(i!=n-5){
-                    temp+=c[pos-1];
-                    i++;
-                }
-                v.push_back(temp);
-                temp.clear();
-                i=0;
-            }
-            num/=10; 
         }
-
-        reverse(v.begin(), v.end());
-        for(auto e:v)
-        temp+=e;
-        return temp;
+        
+        return result;
     }
 };
