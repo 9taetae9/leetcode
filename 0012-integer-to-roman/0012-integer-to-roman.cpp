0e6d5c2869efaa096d4360d9e15a93d0fbec1c8d
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2023-09-20 17:03:52 +0900

    Time: 10 ms (34.46%), Space: 8 MB (39.13%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -4,38 +4,31 @@ public:
         vector<string> v;
         string temp;
         int pos=0;
+        int i=0;
         char c[4]={'I','X','C','M'};
-        
+        char k[3]={'V','L','D'};
         while(num){
             int n = num%10; pos++;  //일의 자리부터 구하기 시작 일,십,백,천 1,2,3,4
             if(n==4||n==9){
                 temp+=c[pos-1];
-                if(n==4){ 
-                    if(pos==1) temp+='V';
-                    else if(pos==2) temp+='L';
-                    else temp+='D';
-                }
-                else{  
-                    if(pos==1) temp+='X';
-                    else if(pos==2) temp+='C';
-                    else temp+='M';
-                }
+                if(n==4)
+                    temp+=k[pos-1];
+                else 
+                    temp+=c[pos];
+
                 v.push_back(temp);
                 temp.clear();
             }
             else if(0<=n&&n<=3){
-                int i=0;
-                
                 while(i!=n){
                     temp+=c[pos-1];
                     i++;
                 }
                 v.push_back(temp);
                 temp.clear();
+                i=0;
             }
             else{
-                int i=0;
-                char k[3]={'V','L','D'};
                 temp+=k[pos-1];
                 while(i!=n-5){
                     temp+=c[pos-1];
@@ -43,9 +36,11 @@ public:
                 }
                 v.push_back(temp);
                 temp.clear();
+                i=0;
             }
             num/=10; 
         }
+
         reverse(v.begin(), v.end());
         for(auto e:v)
         temp+=e;
