da3325303d2dedc7ecb562465df4378594aeba03
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2024-09-23 23:05:17 +0900

    Time: 1 ms (99.4%), Space: 58.8 MB (38.21%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,31 +1,12 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        int n = arr.length;
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n];
+        if(start < 0 || start >= arr.length || arr[start] < 0) return false;
 
-        queue.offer(start);
-        visited[start] = true;
-        
-        while(!queue.isEmpty()){
-            int current = queue.poll();
-            
-            if(arr[current] == 0) return true;
+        if(arr[start] == 0) return true;
 
-            int left = current - arr[current];
-            int right = current + arr[current];
+        int jump = arr[start];
+        arr[start] = -arr[start];
 
-            if(left >= 0 && !visited[left]){
-                queue.offer(left);
-                visited[left] = true;
-            }
-
-            if(right < n && !visited[right]){
-                queue.offer(right);
-                visited[right] = true;
-            }
-        }
-
-        return false;
+        return canReach(arr, start+jump) || canReach(arr, start-jump);
     }
 }
