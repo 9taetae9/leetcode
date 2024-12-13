b22c7c81a1e2f27c06e9ec78a0ccf363f4cf100f
TaeHyoun Kim <kth9903@gmail.com>
2024-12-13 12:48:26 +0900

    Time: 2 ms (99.43%), Space: 48.4 MB (10.42%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -6,18 +6,41 @@ class Solution {
             return list;
         }
 
-        int threshold = nums.length / 3;
-
-        HashMap<Integer,Integer> map = new HashMap<>(nums.length /2 + 1);
+// 1단계: 후보 탐색
+        int can1 = 0; int can2 = 0;
+        int cnt1 = 0; int cnt2 =0;
 
         for(int num : nums){
-            map.merge(num, 1, Integer::sum); //put, getOrDefault 동시 처리
-
-            if(map.get(num)==threshold+1){
-                list.add(num);
+            if(num == can1){
+                cnt1++;
+            }else if(num == can2){
+                cnt2++;
+            }else if(cnt1 == 0){
+                can1 = num;
+                cnt1=1;
+            }else if(cnt2 == 0){
+                can2 = num;
+                cnt2=1;
+            }else{
+                cnt1--;
+                cnt2--;
             }
         }
-        
+
+// 2단계 : 후보 검증
+        cnt1 = 0; cnt2 = 0;
+        for(int num : nums){
+            if(num == can1) {
+                cnt1++; 
+            }else if(num == can2){
+                cnt2++;
+            }
+        }
+
+// 3단계 : 결과 생성
+        if(cnt1 > nums.length / 3) list.add(can1);
+        if(cnt2 > nums.length / 3) list.add(can2);
+
         return list;
     }
 }
