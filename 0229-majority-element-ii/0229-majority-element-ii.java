397366f80612c1a8bfcfb7bdb4b3db41d465f59d
TaeHyoun Kim <kth9903@gmail.com>
2024-12-13 10:44:14 +0900

    Time: 12 ms (25.69%), Space: 47.6 MB (69.79%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,20 +1,23 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+
+        if(nums == null || nums.length == 0){
+            return list;
+        }
+
         int threshold = nums.length / 3;
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
 
         for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0)+1);
-        }
+            map.merge(num, 1, Integer::sum); //put, getOrDefault 동시 처리
 
-        List<Integer> list = new ArrayList<>();
-        for(Map.Entry<Integer,Integer> e : map.entrySet()){
-            if(e.getValue() > threshold){
-                list.add(e.getKey());
+            if(map.get(num)==threshold+1){
+                list.add(num);
             }
         }
-
+        
         return list;
     }
 }
