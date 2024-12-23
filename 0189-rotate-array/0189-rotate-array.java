5a7976bffd08a941383a91beac250e4987e97598
TaeHyoun Kim <kth9903@gmail.com>
2024-12-23 15:21:50 +0900

    Time: 1 ms (49.71%), Space: 57.3 MB (55.43%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,13 +4,10 @@ class Solution {
         int[] nums2 = new int[len];
         int shift = k % len;
 
-        for(int i=0; i < len - shift ; i++){
-            nums2[i+shift] = nums[i];
+        for(int i=0; i<len; i++){
+            nums2[(i+shift)%len] = nums[i];
         }
-        for(int i=len-shift; i<len; i++){
-            nums2[i-(len-shift)] = nums[i];
-        }
-
+        
         System.arraycopy(nums2, 0, nums, 0, len);
     }
 }
