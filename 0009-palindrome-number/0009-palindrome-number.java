cd24172768cd7df1215565d1bc2f0d188a1fbac7
TaeHyoun Kim <kth9903@gmail.com>
2024-12-30 09:53:20 +0900

    Time: 6 ms (31.57%), Space: 44.8 MB (6.66%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -6,8 +6,8 @@ class Solution {
         String s = String.valueOf(x);
         int i = 0;
         int j = s.length()-1;
-        while(i < j && s.charAt(i++) == s.charAt(j--)){
-
+        while(i < j && s.charAt(i) == s.charAt(j)){
+            i++; j--;
         }
 
         return i >= j && s.charAt(i) == s.charAt(j);
