22f33277ed74bcfdd89f5887219a1dcfe0c21680
TaeHyoun Kim <kth9903@gmail.com>
2024-12-30 10:00:17 +0900

    Time: 6 ms (31.57%), Space: 44.5 MB (9.67%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0) return false;
-        if(x < 10) return true;
 
         String s = String.valueOf(x);
         int i = 0;
