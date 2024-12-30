164c4d323f548f946418e1b3fc3da72ba806488a
TaeHyoun Kim <kth9903@gmail.com>
2024-12-30 15:47:19 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,10 +1,21 @@
 class Solution {
     public int mySqrt(int x) {
-        long i=0;
-        while(i*i <= x){
-            i++;
+        if(x<=1) return x;
+        int start = 1;
+        int end = x;
+        int res = 0;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            long square = (long)mid * mid;
+            
+            if(square == x){
+                return mid;
+            }else if(square < x){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
         }
-
-        return (int)i-1;
+        return res;
     }
 }
