2b583541b69d03a299b3023afdadc17f80990108
TaeHyoun Kim <kth9903@gmail.com>
2024-12-30 15:42:03 +0900

    Time: 20 ms (8.55%), Space: 41.2 MB (23.84%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,8 +1,8 @@
 class Solution {
     public int mySqrt(int x) {
-        long i;
-        for(i=0; i * i <= x; i++){
-            ;
+        long i=0;
+        while(i*i <= x){
+            i++;
         }
 
         return (int)i-1;
