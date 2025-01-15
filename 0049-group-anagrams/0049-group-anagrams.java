ef9c0a65bff1e9d6636da0ecb1270bd81b4db494
TaeHyoun Kim <kth9903@gmail.com>
2025-01-15 13:33:08 +0900

    Time: 6 ms (98.03%), Space: 48.1 MB (42.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,23 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>(); //키 : 정렬문자열, 값 : 원본 strs
-
+        Map<String, List<String>> map = new HashMap<>();
         for(String str : strs){
-            int[] count = new int[26];
-            for(char c : str.toCharArray()){
-                count[c-'a']++;
-            }
-
-            StringBuilder sorted = new StringBuilder();
-            for(int freq : count){
-                sorted.append("#").append(freq);
-            }
-            
-            map.putIfAbsent(sorted.toString(), new ArrayList<>());
-            map.get(sorted.toString()).add(str);
+            char[] c_str = str.toCharArray();
+            Arrays.sort(c_str);
+            String s = new String(c_str);
+            List<String> list = map.getOrDefault(s, new ArrayList<>());
+            list.add(str);
+            map.put(s, list);
         }
 
         return new ArrayList<>(map.values());
-        
     }
 }
