3653ef626d6a99f628e732d62172e143e91911de
TaeHyoun Kim <kth9903@gmail.com>
2025-03-27 12:42:25 +0900

    Time: 0 ms (100%), Space: 41.8 MB (66.05%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        for(int i = n-1; i>=0; i--){
+        for(int i = digits.length-1; i>=0; i--){
             if(digits[i]+1 == 10){
                 digits[i] = 0;
             }else{
-                ++digits[i];
+                digits[i]++;
                 return digits;
             }
         }
-
-        int[] newDigits = new int[n+1];
-        newDigits[0] = 1;
-        
-        return newDigits;
+    
+        int[] n_digits = new int[digits.length+1];
+        n_digits[0] = 1;
+        System.arraycopy(n_digits,1, digits,0,digits.length);
+        return n_digits;
     }
 }
