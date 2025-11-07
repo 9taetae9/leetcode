f75349ac514931e80d6892dc18b451c31046330a
TaeHyoun Kim <kth9903@gmail.com>
2025-11-08 08:52:45 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,10 +1,10 @@
-SELECT
-    DISTINCT l1.num AS ConsecutiveNums
-FROM
-    Logs l1
-JOIN
-    Logs l2 ON l1.id = l2.id - 1
-JOIN
-    Logs l3 ON l2.id = l3.id - 1
-WHERE
-    l1.num = l2.num AND l2.num = l3.num;
+WITH NumberedLogs AS (
+    SELECT 
+        num,
+        LAG(num, 1) OVER (ORDER BY id) prev_num,
+        LEAD(num, 1) OVER (ORDER BY id) next_num 
+    FROM Logs
+)
+SELECT num ConsecutiveNums
+FROM NumberedLogs
+WHERE num = prev_num AND num = next_num
